f1f27d60edfe3715cf9ad7818fec29b5f731dbef
Raina <rcho@elitedigital.ca>
2020-01-03 20:41:01 +0900

    resume changes

---
+++ precache-manifest.298855db4f0a8fa039d0b072e2395091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75a3b364767998abd8c983d67fa0b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b136e0638bbfad0dd1b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "905a013c3f3a34c0ddd1",
+    "url": "/static/js/2.280a36ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.280a36ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b136e0638bbfad0dd1b",
+    "url": "/static/js/main.98c35387.chunk.js"
+  },
+  {
+    "revision": "18aa42414906b91ecba3",
+    "url": "/static/js/runtime-main.b89cff06.js"
+  }
+]);
